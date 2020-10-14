d714888aa626abc52b21084eb0580a757d576dc0
Reid <reid@reidchatham.com>
2020-10-14 12:08:17 -0400

    Remove title on front page

--- index.php
+++ index.php
@@ -23,13 +23,13 @@ get_header();
 
 				if ( have_posts() ) :
 
-					if ( is_home() && ! is_front_page() ) :
+					/* if ( is_home() && ! is_front_page() ) :
 						?>
 						<header>
 							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 						</header>
 						<?php
-					endif;
+					endif; */
 
 					/* Masonry Start Section */
 					do_action('prefer_masonry_start_hook'); 
